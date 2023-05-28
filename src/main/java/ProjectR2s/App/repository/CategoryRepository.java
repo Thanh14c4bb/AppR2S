26bfeff84723ff698658296d0d0f5299ddebc723
Thanh14c4bb <121176107+Thanh14c4bb@users.noreply.github.com>
2023-05-28 15:23:09 +0700

    Delete src directory

--- src/main/java/ProjectR2s/App/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package ProjectR2s.App.repository;
-
-import ProjectR2s.App.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
